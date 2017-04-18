edee782903c3bf9e119ff6c99b5fa9411bf035ad
Igor <lozaigor88@gmail.com>
2017-04-18 17:02:29 +0300

    added  quad

--- ProjectOpenGL/Source.cpp
+++ ProjectOpenGL/Source.cpp
@@ -6,7 +6,15 @@ void Render()
    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
    glClear(GL_COLOR_BUFFER_BIT);
 
-   glFlush();
+   glBegin(GL_QUADS);
+   glColor3f(0.0f, 1.0f, 0.0f);
+   glVertex3f(-20, -20, 0);
+   glVertex3f(20, -20, 0);
+   glVertex3f(20, 20, 0);
+   glVertex3f(-20, 20, 0);
+   glEnd();
+
+   glutSwapBuffers();
 }
 
 
@@ -21,12 +29,14 @@ void Init()
 int main()
 {
    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-   glutInitWindowSize(600, 600);
+   glutInitWindowSize(800, 600);
    glutInitWindowPosition(0, 0);
-   glutCreateWindow("lesson");
+   glutCreateWindow("OpenGL example");
+   
    glutDisplayFunc(Render);
    Init();
    glutMainLoop();
 
    return 0;
 }
+;
